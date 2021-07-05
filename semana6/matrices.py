077e5c1c6dca64a1aedb810c939690206b57801b
juanjmaidana <maidanajj@gmail.com>
2021-07-05 13:07:02 -0300

    matrices

--- semana6/matrices.py
+++ semana6/matrices.py
@@ -1,2 +1,28 @@
-numero = 10
-numeroIngresado = int(input("Ingresar nro: "))
+# Ejemplo de Lista "mutable" puedo agregar y modificar
+
+# cont = 0
+# lista = []
+
+# while(True):
+#     ingNum = int(input("Ingrese un numero a la matriz: "))
+#     if(ingNum == 0):
+#         break
+#     lista.append(ingNum)
+#     cont = cont + 1
+
+# for i in range(cont):
+#     print(lista[i])
+
+#TUPLA inmutable no puedo cambiar.
+
+# tuplaVariante = (23,15,20,3)
+# print(tuplaVariante)
+# print(tuplaVariante[3])
+
+#Diccionarios parcialmente inmutables
+
+datosPersonales = {"Nombre:": "Juan José", "Apellido ": "Maidana", "Edad:":40, "Legajo":1000}
+datosPersonales["Edad:"] = input("Ingrese edad:")
+print(datosPersonales)
+print(datosPersonales["Nombre:"])
+
