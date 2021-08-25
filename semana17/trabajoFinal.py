849ac990358283b9ff3c2d395aa906361f45b609
maidanajj <maidanajj@gmail.com>
2021-08-24 21:57:31 -0300

    gui

--- semana17/trabajoFinal.py
+++ semana17/trabajoFinal.py
@@ -18,22 +18,35 @@ def consulta():
     listaProduc = consultaWeb.json()
     return(listaProduc)
 
+productos = consulta()
 
-raiz = Tk()
-raiz.geometry("300x200")
-raiz.title("Carga de Pedido")
+#print(productos['productos'][0]['nombre'])
 
+ventana = Tk()
+ventana.geometry("300x200")
+ventana.title("Formulario Simple")
 miFrame= Frame()
 miFrame.pack()
-
 bienvenido = Label(miFrame, text="BIENVENIDO")
 bienvenido.grid(row=0, column=0)
 bienvenido.config(font=('Arial', 16))
-
+#-----Seccion de Nombre-----
 nombre_label= Label(miFrame, text="Cual es tu nombre:")
 nombre_label.grid(row=1, column=0)
 nombre_label.config(padx=10, pady=10)
 cuadro_nombre=Entry(miFrame)
 cuadro_nombre.grid(row=1, column=1)
+#-----Seccion de Apellido-----
+apellido_label=Label(miFrame, text="Cual es tu apellido: ")
+apellido_label.grid(row=2, column=0)
+apellido_label.config(padx=10, pady=10)
+cuadro_Apellido=Entry(miFrame)
+cuadro_Apellido.grid(row=2, column=1)
+#-----Seccion de Dirección-----
+direccion=Label(miFrame, text="Dirección: ")
+direccion.grid(row=3, column=0)
+direccion.config(padx=10, pady=10)
+cuadro_Direccion=Entry(miFrame)
+cuadro_Direccion.grid(row=3, column=1)
 
-raiz.mainloop()
+ventana.mainloop()
